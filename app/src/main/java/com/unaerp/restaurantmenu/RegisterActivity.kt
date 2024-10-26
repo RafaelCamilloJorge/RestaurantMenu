6abc59d5c65b0a73a57e6d7fe829a02c141b3af8
Rafael Jorge <rafaelcamillojorge@gmail.com>
2024-10-26 11:12:51 -0300

    feature: Created login and register

---
+++ app/src/main/java/com/unaerp/restaurantmenu/RegisterActivity.kt
@@ -0,0 +1,54 @@
+package com.unaerp.restaurantmenu
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.unaerp.restaurantmenu.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            val name = binding.etName.text.toString().trim()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty()) {
+                createUserWithEmailAndPassword(email, password)
+            } else {
+                Toast.makeText(this, "Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun createUserWithEmailAndPassword(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Toast.makeText(this, "Registro realizado com sucesso!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(baseContext, "Falha ao registrar: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
