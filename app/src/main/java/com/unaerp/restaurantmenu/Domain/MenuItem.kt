c9066f15887735030128025b6b66c3d0fa934960
Felipe Lemos Bellini <felipe.bellini@stuo.app>
2024-11-04 22:49:38 -0300

    feature: get totalValue ShoppingCar

--- app/src/main/java/com/unaerp/restaurantmenu/Domain/MenuItem.kt
+++ app/src/main/java/com/unaerp/restaurantmenu/Domain/MenuItem.kt
@@ -7,23 +7,15 @@ class MenuItem(
     val price: Double,
     val image: String
 ) {
-    fun fromCollection(item: Map<String, Any>): MenuItem {
-        return MenuItem(
-            (item["id"] ?: 0) as Int,
-            (item["name"] ?: "").toString(),
-            (item["description"] ?: "") as String,
-            (item["price"] ?: 0.0) as Double,
-            (item["image"] ?: "") as String
-        )
-    }
-
-    fun toMap(): Map<String, Any> {
-        return mapOf(
-            "id" to id,
-            "name" to name,
-            "description" to description,
-            "price" to price,
-            "image" to image
-        )
+    companion object {
+        fun fromCollection(item: Map<String, Any>): MenuItem {
+            return MenuItem(
+                (item["id"] ?: 0) as Int,
+                (item["name"] ?: "").toString(),
+                (item["description"] ?: "") as String,
+                (item["price"] ?: 0.0) as Double,
+                (item["image"] ?: "") as String
+            )
+        }
     }
 }
